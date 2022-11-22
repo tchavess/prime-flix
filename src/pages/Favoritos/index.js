63b2b2766eebfde7f63689e9cfe5aa5cf2f192e5
tchavess <thiago.chaves04@gmail.com>
2022-11-22 13:08:29 -0300

    feat: Favorite page dynamic

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react"
+import './favoritos.css'
+import { Link } from 'react-router-dom'
 export default function Favoritos(){
+
+    const [filmes, setFilmes] = useState([])
+    useEffect(() => {
+        const minhaLista = localStorage.getItem("@primeflix")
+        let filmesSalvos = minhaLista || []
+        setFilmes(JSON.parse(filmesSalvos))
+
+    })
+
+    function excluiFilmeDaLista(id ){
+        const filmeFiltro = filmes.filter( (filme) => filme.id !== id ) 
+        setFilmes(filmeFiltro)
+        localStorage.setItem("@primeflix", JSON.stringify(filmeFiltro))
+        alert("Filme excluido com sucesso")
+    }
     return(
-        <div>
-            <h1>FAVORITOS!</h1>
+        <div className="meus-filmes">
+            <h1>Meus filmes</h1>
+            <ul>
+                {filmes.map((filme) => {
+                return(
+                    <li key={filme.id}>
+                    <span>{filme.title}</span>
+
+                    <div>
+                        <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
+                        <button onClick={() => excluiFilmeDaLista(filme.id)}>Excluir</button>
+                    </div>
+                    </li>
+                )
+                })}
+            </ul>
         </div>
     )
 }
--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -35,14 +35,29 @@ function Filme() {
         console.log("COMPONENTE DESMONTADO!")
        }
    },[id, navigate])
+   
+   function salvaFilme(){
+    const minhaLista = localStorage.getItem("@primeflix")
+    let filmesSalvos = JSON.parse(minhaLista) || []
+    const hasFilme = filmesSalvos.some((filmeSalvo) => filmeSalvo.id === filme.id)
+    if(hasFilme){
+        alert("Este filme ja esta na sua lista.")
+        return
+    }
+    
+    filmesSalvos.push(filme)
+    localStorage.setItem("@primeflix",JSON.stringify(filmesSalvos))
+    alert("Filme salvo com sucesso!")
+    
+   }
 
    if(loading){
-        return(
-            <div className="loading">
-                <h2>Carregando detalhes do filme...</h2>
-            </div>
-        )
-   }
+    return(
+        <div className="filme-info">
+            <h2>Carregando detalhes do filme...</h2>
+        </div>
+    )
+    }
     return(
         <div className='filme-info'>
             <h1>{filme.title}</h1>
@@ -52,8 +67,8 @@ function Filme() {
             <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
 
             <div className='area-buttons'>
-                <button>Salvar</button>
-                <a target="_blank" rel="external noreferrer" href={`https://www.youtube.com/results?search_query=${filme.title} trailer`} >Trailer</a>
+                <button onClick={salvaFilme}>Salvar</button>
+                <a target="blank" rel="external noreferrer" href={`https://www.youtube.com/results?search_query=${filme.title} trailer`} >Trailer</a>
             </div>
         </div>
     )
